8382ff13cdc0f71e69ece76fb0f6d99d372ab2d7
Peter C <ptrcortes@email.arizona.edu>
2015-09-17 13:36:32 -0700

    second commit using script

---
+++ src/server/ChattRoom.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * This object is the server that does the communicating with the clients. It
+ * distributes messages and handles events such as user connection and
+ * disconnection.
+ *
+ * @author Peter Cortes
+ */
+public class ChattRoom
+{
+
+}
