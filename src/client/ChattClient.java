ea6e6cb241876312ed458465bff17205a3d3df76
Peter C <ptrcortes@email.arizona.edu>
2015-09-17 13:23:13 -0700

    initial commit

---
+++ src/client/ChattClient.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ *
+ *
+ * @author Peter Cortes
+ */
+public class ChattClient
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/server/ChattHypervisor.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ *
+ *
+ * @author Peter Cortes
+ */
+public class ChattHypervisor
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
